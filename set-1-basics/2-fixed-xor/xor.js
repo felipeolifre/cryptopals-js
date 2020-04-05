72a7e2ecfe8ae11fcb70f66a25a17ac7ce7a3451
Felipe Olivera <felipe.olivera@nextroll.com>
2020-04-04 21:07:26 -0300

    Add set-1 challenge-2 solution

---
+++ set-1-basics/2-fixed-xor/xor.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function xor(a, b) {
+    var length = Math.max(a.length, b.length);
+    var buffer = Buffer.allocUnsafe(length);
+
+    for (var i = 0; i < length; ++i) {
+        buffer[i] = a[i] ^ b[i];
+    }
+
+    return buffer;
+}
+
+function hexXor(hexString1, hexString2) {
+    const buffer1 = Buffer.from(hexString1, "hex");
+    const buffer2 = Buffer.from(hexString2, "hex");
+    return xor(buffer1, buffer2).toString("hex");
+}
+
+rl.question("What's the first hex string?\n", (hexString1) => {
+    rl.question("What's the second hex string?\n", (hexString2) => {
+        console.log("The XOR operation produced:");
+        console.log(hexXor(hexString1, hexString2));
+        rl.close();
+    });
+});
